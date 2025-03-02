67d5746eba68ff48cfb91a01bce9c16bec97d98e
has-bi <hassadiqin.31@gmail.com>
2025-03-02 15:09:30 +0700

    chore: add mobile first and global styling for button, and input and form

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,31 @@
-import { Plus_Jakarta_Sans } from 'next/font/google'
-import './globals.css'
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
-  variable: '--font-plus-jakarta',
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-})
+  variable: "--font-plus-jakarta",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export const metadata = {
-  title: 'TheraMind',
-  description: 'your mindfulnes app',
-}
+  title: "TheraMind",
+  description: "your mindfulnes app",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${plusJakartaSans.variable} antialiased`}>{children}</body>
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </head>
+      <body className={`${plusJakartaSans.variable} antialiased`}>
+        <div className="mobile-container w-full max-w-[480px] bg-white min-h-screen">
+          {children}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
