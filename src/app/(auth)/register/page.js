d2a11e267df5d8ac7230d72486d02f40301c194d
khmuhtadin <contactmuhtadin@gmail.com>
2025-03-02 12:10:53 +0700

    change default globalcss

--- src/app/(auth)/register/page.js
+++ src/app/(auth)/register/page.js
@@ -1,5 +1,19 @@
-import React from 'react'
+'use client'
+import { useActionState } from 'react'
+import { registerAction } from './action'
 
 export default function page() {
-  return <div>page</div>
+  const [state, formAction, pending] = useActionState(registerAction, null)
+
+  return (
+    <div>
+      <form action={registerAction}>
+        <input name="firstname" placeholder="firstname" />
+        <input name="lastname" placeholder="lastname" />
+        <input name="email" type="email" placeholder="email" />
+        <input name="password" type="password" placeholder="password" />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  )
 }
