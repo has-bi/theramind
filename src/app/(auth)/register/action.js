3fce9324f92ca93718e88819fc07b718ed9045f2
khmuhtadin <contactmuhtadin@gmail.com>
2025-03-02 12:27:56 +0700

    add prisma client in utils

--- src/app/(auth)/register/action.js
+++ src/app/(auth)/register/action.js
@@ -1,10 +1,18 @@
 'use server'
 
 export async function registerAction(_, formData) {
-  const firstName = await formData.get('firstname')
-  const lastName = await formData.get('lastname')
-  const email = await formData.get('email')
-  const password = await formData.get('password')
+  try {
+    const firstName = await formData.get('firstname')
+    const lastName = await formData.get('lastname')
+    const email = await formData.get('email')
+    const password = await formData.get('password')
 
-  console.log({ firstName, lastName, email, password })
+    if (!firstName || !lastName || !email || !password) {
+      throw new Error('Missing form data fields')
+    }
+
+    console.log({ firstName, lastName, email, password })
+  } catch (error) {
+    console.error('Error reading form data:', error)
+  }
 }
