c61bf81fc090406f9b15fd0cf298f378194c3425
khmuhtadin <contactmuhtadin@gmail.com>
2025-03-02 13:37:48 +0700

    add logic  register feature

--- src/app/(auth)/register/action.js
+++ src/app/(auth)/register/action.js
@@ -10,8 +10,28 @@ export async function registerAction(_, formData) {
     const email = await formData.get('email')
     const password = await formData.get('password')
 
-    const hashedPassword = await bcrypt.hash(password, 12)
+    if (!firstName || !lastName || !email || !password) {
+      return {
+        success: false,
+        message: 'All fields are required!',
+      }
+    }
 
+    //check collision
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (existingUser) {
+      return {
+        success: false,
+        message: 'User already registered!',
+      }
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12)
     const user = await prisma.user.create({
       data: {
         firstName,
@@ -21,7 +41,10 @@ export async function registerAction(_, formData) {
       },
     })
 
-    console.log({ firstName, lastName, email, password, hashedPassword })
+    return {
+      success: true,
+      message: 'User registered successfully!',
+    }
   } catch (error) {
     console.error('Error reading form data:', error)
   }
