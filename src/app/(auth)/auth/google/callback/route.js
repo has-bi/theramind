bb88005909b66dcfeba11111db544b8f1d7afbca
khmuhtadin <contactmuhtadin@gmail.com>
2025-03-03 17:31:37 +0700

    slicing name from google to firstname & lastname

--- src/app/(auth)/auth/google/callback/route.js
+++ src/app/(auth)/auth/google/callback/route.js
@@ -3,11 +3,9 @@ import { prisma } from "@/utils/prisma";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default async function Page({ searchParams }) {
-  const query = await searchParams;
-
-  const state = query.state;
-  const code = query.code;
+export async function GET(req) {
+  const query = req.nextUrl.searchParams;
+  const code = query.get("code");
 
   const cookieStore = await cookies();
   const codeVerifier = cookieStore.get("codeVerifier")?.value;
@@ -30,15 +28,33 @@ export default async function Page({ searchParams }) {
     },
   });
 
+  let newUser = user;
+
   //ini kalo gaada, yaudah buat usertannpa password
   if (!user) {
-    const newUser = await prisma.user.create({
+    // split name from google separated by space
+    const nameParts = data.name.split(" ");
+    const firstName = nameParts[0];
+    const lastName = nameParts.slice(1).join(" ");
+
+    newUser = await prisma.user.create({
       data: {
-        name: data.name,
+        firstName,
+        lastName,
         email: data.email,
+        googleId: data.sub,
       },
     });
   }
+
+  //buat Session
+  const newSession = await prisma.session.create({
+    data: {
+      userId: newUser.id,
+      expires: new Date(Date.now() + 60 * 60 * 24 * 7 * 1000),
+      data: JSON.stringify({ accessToken }),
+    },
+  });
 
   //buat cookie
   cookieStore.set("sessionId", newSession.id, {
