15f8432ddccda716d84a222b9a71cb6350fc3426
khmuhtadin <contactmuhtadin@gmail.com>
2025-03-03 22:09:59 +0700

    add await in validateSession

--- src/app/(auth)/login/action.js
+++ src/app/(auth)/login/action.js
@@ -7,7 +7,7 @@ import { redirect } from "next/navigation";
 
 export async function validateSession() {
   const cookieStore = cookies();
-  const sessionId = cookieStore.get("sessionId")?.value;
+  const sessionId = await cookieStore.get("sessionId")?.value;
 
   if (sessionId) {
     const session = await prisma.session.findUnique({
