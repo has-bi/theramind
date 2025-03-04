343eb13c768bac8879ef9f98a596cfde80ecabed
khmuhtadin <contactmuhtadin@gmail.com>
2025-03-04 09:32:49 +0700

    remove duplicated files and folders

--- src/app/(mainapp)/dashboard/page.js
+++ src/app/(mainapp)/dashboard/page.js
@@ -1,3 +1,5 @@
 export default async function Page() {
+  // i am deleting duplicated files and folders
+  // add this to make the commit works because
   return <div>Temporary (protected) Dashboard Page</div>;
 }
