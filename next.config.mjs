173037084ac1a4c38350e22b58dfb5e3d52aea94
khmuhtadin <contactmuhtadin@gmail.com>
2025-03-06 22:07:10 +0700

    confuse with 2 method

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,9 @@
-/** @type {import('next').NextConfig} */
-
 import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
 };
 
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-});
-
-// Merge MDX config with Next.js config
+const withMDX = createMDX({});
 export default withMDX(nextConfig);
