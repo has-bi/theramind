b39eef515b0fe74de96b43bb7672346835a6be5d
taufiqrohmand <taufiqrohmand@gmail.com>
2025-03-07 20:21:28 +0700

    add calendar

--- src/app/(mainapp)/dashboard/page.js
+++ src/app/(mainapp)/dashboard/page.js
@@ -1,5 +1,15 @@
+import CalendarMoodView from "./calendarMoodView";
+
 export default async function Page() {
   // i am deleting duplicated files and folders
   // add this to make the commit works because
-  return <div>Temporary (protected) Dashboard Page</div>;
+  return (
+    <div>
+      Temporary (protected) Dashboard Page
+      <header className="font-bold text-3xl text-center">Dashboard</header>
+      <main>
+        <CalendarMoodView />
+      </main>
+    </div>
+  );
 }
