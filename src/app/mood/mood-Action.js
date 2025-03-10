f8ea70433607a2cb6861253223eb8032f060192c
taufiqrohmand <taufiqrohmand@gmail.com>
2025-03-10 15:52:15 +0700

    save daily mood

--- src/app/mood/mood-Action.js
+++ src/app/mood/mood-Action.js
@@ -1,13 +1,47 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import { prisma } from "../utils/prisma";
 
 export async function createMoodAction(_, formData) {
-  // const userId = formData.get("userId");
-  const emotionId = Number(formData.get("emotionId"));
+  const cookieStore = await cookies();
+  const sessionId = cookieStore.get("sessionId")?.value;
 
-  //   revalidatePath("/");
-  console.log(emotionId);
-  redirect("/journal");
+  if (!sessionId) {
+    return redirect("/login");
+  }
+  const session = await prisma.session.findUnique({
+    where: { id: sessionId },
+    include: { user: true },
+  });
+  const userId = session.user.id;
+  const emotionId = formData.get("emotionId");
+
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  const existingEntry = await prisma.moodEntry.findFirst({
+    where: {
+      userId: userId,
+      createdAt: {
+        gte: today,
+        lt: new Date(today.getTime() + 24 * 60 * 60 * 1000),
+      },
+    },
+  });
+
+  if (!existingEntry) {
+    await prisma.moodEntry.create({
+      data: {
+        userId,
+        emotionId,
+        createdAt: new Date(),
+      },
+    });
+
+    redirect("/journal");
+  }
+
+  redirect("/dashboard");
 }
